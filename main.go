150f024ac204624a5d150b188d85ed3bb9f0e84c
Nur Ahmad Khatim <naimackerman@gmail.com>
2023-12-01 09:21:16 +0700

    update main.go

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type Mobil struct {
-	Roda                  [4]string
-	PintuKanan, PintuKiri Pintu
+	Roda  [4]string
+	Pintu [2]Pintu
 }
 
 type Pintu struct {
@@ -12,50 +15,71 @@ type Pintu struct {
 	BunyiBuka  string
 }
 
-func (m *Mobil) GantiBan(ban [4]string) {
-	for i := 0; i < 4; i++ {
-		m.Roda[i] = ban[i]
+type Ban struct {
+	Karet string
+	Besi  string
+	Kayu  string
+}
+
+func (m *Mobil) GantiBan(ban ...string) {
+	copy(m.Roda[:], ban[:])
+}
+
+func (m *Mobil) KetukPintu(sisi string) {
+	sisi = strings.ToLower(sisi)
+	if sisi == "kanan" {
+		fmt.Println("Ketuk Pintu Kanan:", m.Pintu[0].BunyiKetuk)
+	} else if sisi == "kiri" {
+		fmt.Println("Ketuk Pintu Kiri:", m.Pintu[1].BunyiKetuk)
 	}
 }
 
-func (m *Mobil) KetukDanBukaPintu() {
-	fmt.Println("Ketuk Pintu Kanan:", m.PintuKanan.BunyiKetuk)
-	fmt.Println("Buka Pintu Kanan:", m.PintuKanan.BunyiBuka)
-
-	fmt.Println("Ketuk Pintu Kiri:", m.PintuKiri.BunyiKetuk)
-	fmt.Println("Buka Pintu Kiri:", m.PintuKiri.BunyiBuka)
+func (m *Mobil) BukaPintu(sisi string) {
+	sisi = strings.ToLower(sisi)
+	if sisi == "kanan" {
+		fmt.Println("Buka Pintu Kanan:", m.Pintu[0].BunyiBuka)
+	} else if sisi == "kiri" {
+		fmt.Println("Buka Pintu Kiri:", m.Pintu[1].BunyiBuka)
+	}
 }
 
 func main() {
-	ban := [4]string{"Ban Karet", "Ban Karet", "Ban Karet", "Ban Karet"}
+	ban := Ban{
+		Karet: "Ban Karet",
+		Besi:  "Ban Besi",
+		Kayu:  "Ban Kayu",
+	}
 	mobil := Mobil{
-		Roda: ban,
-		PintuKanan: Pintu{
-			BunyiKetuk: "Knock",
-			BunyiBuka:  "beep",
+		Roda: [4]string{
+			ban.Besi,
+			ban.Besi,
+			ban.Besi,
+			ban.Besi,
 		},
-		PintuKiri: Pintu{
-			BunyiKetuk: "beep",
-			BunyiBuka:  "Knock",
+		Pintu: [2]Pintu{
+			{
+				BunyiKetuk: "Knock",
+				BunyiBuka:  "Beep",
+			},
+			{
+				BunyiKetuk: "Beep",
+				BunyiBuka:  "Knock",
+			},
 		},
 	}
 
-	fmt.Println("Mobil Awal:")
-	mobil.KetukDanBukaPintu()
-	for i := 0; i < 4; i++ {
-		fmt.Println("Roda ke-", i+1, ":", mobil.Roda[i])
+	mobil.BukaPintu("KaNaN")
+	mobil.BukaPintu("KIRI")
+	mobil.KetukPintu("kanan")
+	mobil.KetukPintu("kiRI")
+
+	fmt.Println("Ban mobil sebelum diganti:")
+	for i := 0; i < len(mobil.Roda); i++ {
+		fmt.Println("Ban mobil roda ke-", i+1, ":", mobil.Roda[i])
 	}
-
-	// Ganti ban mobil
-	ban[0] = "Ban Besi"
-	ban[1] = "Ban Kayu"
-	ban[2] = "Ban Karet"
-	ban[3] = "Ban Besi"
-	mobil.GantiBan(ban)
-
-	fmt.Println("\nMobil Setelah Ganti Ban:")
-	mobil.KetukDanBukaPintu()
-	for i := 0; i < 4; i++ {
-		fmt.Println("Roda ke-", i+1, ":", mobil.Roda[i])
+	mobil.GantiBan(ban.Karet, ban.Kayu, ban.Karet, ban.Kayu)
+	fmt.Println("Ban mobil setelah diganti:")
+	for i := 0; i < len(mobil.Roda); i++ {
+		fmt.Println("Ban mobil roda ke-", i+1, ":", mobil.Roda[i])
 	}
 }
